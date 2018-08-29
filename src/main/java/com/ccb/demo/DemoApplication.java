344ada38322482b964e5302ebcf6de278acc4f34
黑眈 <heidan@ccbtrust.com.cn>
2018-08-29 11:38:31 +0800

    add

---
+++ src/main/java/com/ccb/demo/DemoApplication.java
@@ -0,0 +1,19 @@
+package com.ccb.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class DemoApplication {
+	@RequestMapping("/")
+	String index() {
+		return "Hello demo";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+}
